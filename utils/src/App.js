e4adefff28c3409722c75deebabd977963f5fd6a
Sri-sti <shrishtichauhan13@gmail.com>
2024-07-09 21:04:09 +0530

    improved dark mode

--- utils/src/App.js
+++ utils/src/App.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import About from './components/About';
-//import Textform from './components/Textform';
+//import About from './components/About';
+import Textform from './components/Textform';
 function App() {
+  //creting state for dark mode
+  const[mode,setMode]=useState("light");
+  const toggmode=()=>{
+    if(mode==="light"){
+      setMode("dark")
+      document.body.style.backgroundColor='#042743';
+        }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor='white';
+    }
+  }
   return(
     <>
    
-<Navbar title="Utils" about="About us"/>
+<Navbar title="Utils" about="About us" Mod={mode} togglemode={toggmode} />
 <div className='container my-3'>
-{/* <Textform heading="Enter the text"/> */}
-<About/>
+<Textform heading="Enter the text" Mod={mode}/>
+{/* <About/> */}
 </div>
     </>
   )
--- utils/src/components/Textform.js
+++ utils/src/components/Textform.js
@@ -21,6 +21,19 @@ export default function Textformprops(props) {
       setText(tex);
   }
 
+  //Function to copy the text to clipborard
+  const handlecopy=()=>{
+    let tex=document.getElementById("myBox");
+    tex.select();
+    navigator.clipboard.writeText(tex.value)
+}
+
+ //Function to handle extra space
+ const handlspace=()=>{
+  let tex=text.split(/[ ]+/);
+  setText(tex.join(" "));
+}
+
    //Function to clear the text
    const handlclear=()=>{
     let tex=" ";
@@ -29,21 +42,23 @@ export default function Textformprops(props) {
 
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color:props.Mod==='dark'?'white':'#042743'}}>
       <div className="mb-3">
         <h1>{props.heading}</h1>
-      <textarea className="form-control" value={text} id="myBox" rows="8" onChange={handleonchange}></textarea>
+      <textarea className="form-control" value={text} id="myBox" rows="8" style={{backgroundColor:props.Mod==='dark'?'#7696B0':'white',color:props.Mod==='dark'?'white':'#042743'}} onChange={handleonchange}></textarea>
       </div>
       <button className='btn btn-primary mx-2' onClick={handleup}>Uppercase</button>
       <button className='btn btn-primary mx-2' onClick={handlelw}>Lowercase</button>
+      <button className='btn btn-primary mx-2' onClick={handlecopy}>Copy</button>
+      <button className='btn btn-primary mx-2' onClick={handlspace}>Space</button>
       <button className='btn btn-primary' onClick={handlclear}>Clear text</button>
     </div>
-    <div className='container my-3'>
+    <div className='container my-3' style={{color:props.Mod==='dark'?'white':'#042743'}}>
       <h1>Your Text Summarry</h1>
       <p>{text.split(" ").length} words , {text.length} characters</p>
       <p>{0.008*text.split(" ").length} minutes read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter your text for preview"}</p>
     </div>
     </>
   )
